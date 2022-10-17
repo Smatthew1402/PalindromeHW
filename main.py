3c6182315bdb3a43450e5ae81c6972b43026bec3
Smatthew1402 <100808047+Smatthew1402@users.noreply.github.com>
2022-10-17 18:38:59 -0400

    V0 initial build
    initial structure of the checker laid out

---
+++ main.py
@@ -0,0 +1,14 @@
+class PalinChecker:
+    def __init__(self):
+        pass
+
+    def isDrome(self, word):
+        queue = []
+        pass
+
+
+    def fillqueue(self, word, queue):
+        for letter in word:
+            pass
+        pass
+    
