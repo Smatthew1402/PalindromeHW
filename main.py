5f9575872c1f2a3d51b06e9276391c745260e897
Smatthew1402 <100808047+Smatthew1402@users.noreply.github.com>
2022-10-17 19:14:45 -0400

    V1, Completed program
    Tests palindrome check with 10 palindromes and 10 non-palindromes

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
+from collections import deque
+
+
 class PalinChecker:
     def __init__(self):
         pass
 
-    def isDrome(self, word):
-        queue = []
-        pass
-
+    def isdrome(self, word):
+        isdrome = True
+        rightqueue = deque([])
+        rightqueue = self.fillqueue(word.lower(), rightqueue)
+        leftqueue = deque([])
+        for num in range(int(len(rightqueue)/2)):
+            leftqueue.append(rightqueue.popleft())
+        leftqueue.reverse()
+        if len(word) % 2 == 1:
+            rightqueue.popleft()
+        for num in range(len(rightqueue)):
+            if isdrome:
+                isdrome = (rightqueue.popleft() == leftqueue.popleft())
+        return isdrome
 
     def fillqueue(self, word, queue):
         for letter in word:
-            pass
-        pass
-    
+            queue.append(letter)
+        return queue
+
+
+if __name__ == '__main__':
+    pc = PalinChecker()
+    print("Commencing Positive Tests:\n")
+    print("1. racecar , True: ", pc.isdrome('racecar'))
+    print("2. kayak , True:", pc.isdrome('kayak'))
+    print("3. deified , True:", pc.isdrome('deified'))
+    print("4. rotator , True:", pc.isdrome('rotator'))
+    print("5. repaper , True:", pc.isdrome('repaper'))
+    print("6. deed , True:", pc.isdrome('deed'))
+    print("7. peep , True:", pc.isdrome('peep'))
+    print("8. wow , True:", pc.isdrome('wow'))
+    print("9. noon , True:", pc.isdrome('noon'))
+    print("10. civic , True:", pc.isdrome('civic'))
+    print("Positive tests complete, ------------------------- \n \nCommencing Negative Tests:\n ")
+    print("1. racecars , False:", pc.isdrome('racecars'))
+    print("2. kayaks , False:", pc.isdrome('kayaks'))
+    print("3. defies , False:", pc.isdrome('defies'))
+    print("4. rotators , False:", pc.isdrome('rotators'))
+    print("5. depaper , False:", pc.isdrome('depaper'))
+    print("6. deeds , False:", pc.isdrome('deeds'))
+    print("7. peeps , False:", pc.isdrome('peeps'))
+    print("8. vow , False:", pc.isdrome('vow'))
+    print("9. moon , False:", pc.isdrome('moon'))
+    print("10. civics , False:", pc.isdrome('civics'))
